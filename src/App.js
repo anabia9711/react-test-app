18eb7abd0d1c33d891eeee29fff9dd9b4414b0d2
Firoz Haider <fhaider@egencia.com>
2025-11-04 17:33:18 +0530

    Added changes commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit by Firoz and save to reload. This is awesome!
+          Edit by Firoz and save to reload. 
         </p>
         <p>This is my website with CI/CD pipeline</p>
         <a
