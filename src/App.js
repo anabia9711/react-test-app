b16c40263a0133aa341cfe973fb0e305ac4d5752
Firoz Haider <fhaider@egencia.com>
2025-11-04 17:00:34 +0530

    new change commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit by Firoz and save to reload.
+          Edit by Firoz and save to reload. This is awesome!
         </p>
         <a
           className="App-link"
